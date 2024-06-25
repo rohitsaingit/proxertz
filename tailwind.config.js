ef8578ab56f796379a8cac86bd91149b35de5933
rohitsaingit <rohit.sain.karnal.gmail.com>
2024-06-25 19:34:10 +0530

    third

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./templates/*"],
+  theme: {
+    extend: {
+      fontFamily: {
+        raleway: ['Raleway', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
+
